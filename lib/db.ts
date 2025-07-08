583b933bf0e6f4c91cebde0916b2a092bd676b28
Francisco Natale <francisco_natale@hotmail.com>
2025-07-07 22:14:00 -0300

    change connection w/ db

--- lib/db.ts
+++ lib/db.ts
@@ -11,11 +11,7 @@ export async function getDataSource() {
 
   AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
     entities: [Contact],
     synchronize: true,
   });
